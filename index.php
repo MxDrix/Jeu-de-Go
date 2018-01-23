ec4785450cae352df3dd1a862517fd7e17e962cf
JohnGanem <jonathan.ganem94@gmail.com>
2018-01-23 18:28:38 +0100

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-echo "test";
-?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Jeu de Go</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
