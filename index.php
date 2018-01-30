9c12e46fa0e6dc8cd63773097c86dfa99c05c3da
RICHARD\Richard <richrd.chandon@gmail.com>
2018-01-30 15:13:57 +0100

    Merge branch 'master' of https://github.com/MxDrix/Jeu-de-Go.git
    # Conflicts:
    #	index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 echo "Renaud";
 ?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Jeu de Go</title>
+    </head>
+    <body>
+        <?php
+        // ehrftdjgykhul;im
+        ?>
+    </body>
+</html>
